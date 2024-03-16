c67d81ca5883c95b9304cf104e33258650f499cc
Jhony Yañez <jhony.yaesz27@gmail.com>
2024-03-16 11:37:31 -0600

    houses terminado

--- src/app/maps/components/mini-map/mini-map.component.ts
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, ViewChild,   } from '@angular/core';
+
+import { Map, Marker } from 'mapbox-gl';
+import { MarkersPageComponent } from '../../pages/markers-page/markers-page.component';
+
 
 @Component({
-  selector: 'app-mini-map',
+  selector: 'map-mini-map',
   templateUrl: './mini-map.component.html',
   styleUrl: './mini-map.component.css'
 })
-export class MiniMapComponent {
+export class MiniMapComponent implements AfterViewInit {
 
+
+
+  @ViewChild('map')divMap?: ElementRef ;
+
+  @Input() lngLat?: [number,number];
+
+
+ ngAfterViewInit() {
+
+  if (!this.divMap?.nativeElement) throw 'Map Div not found';
+  if (!this.lngLat) throw "LngLAt Can't be null";
+
+
+  //mapa
+
+const map = new Map({
+  container : this.divMap.nativeElement,
+  style: 'mapbox://styles/mapbox/streets-v12',
+  center : this.lngLat,
+  zoom : 15,
+  interactive :false,
+});
+
+  //marker
+  new Marker()
+  .setLngLat(this.lngLat).
+  addTo(map)
+
+
+}
 }
