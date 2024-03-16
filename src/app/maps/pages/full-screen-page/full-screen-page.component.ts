7be7cf6b0b1788e2565017fc7b5747e461d42115
Jhony Yañez <jhony.yaesz27@gmail.com>
2024-03-16 09:56:12 -0600

    creacion de markadores añadida

--- src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -1,9 +1,8 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
-   import mapboxgl from 'mapbox-gl';    //const mapboxgl = require('mapbox-gl');   //
+   import  {Map} from 'mapbox-gl';    //const mapboxgl = require('mapbox-gl');   //
 
 
-(mapboxgl as any).accessToken = 'pk.eyJ1IjoiYWxvbnNvLXNtaXRoIiwiYSI6ImNsdHExOHJhdDAxOHMyanJpdnczbHpscTkifQ.xqCUzK1YZkenvCghPaJyOQ';
 
 
 @Component({
@@ -11,10 +10,17 @@ import { AfterViewInit, Component } from '@angular/core';
   styleUrl: './full-screen-page.component.css'
 })
 export class FullScreenPageComponent implements AfterViewInit {
+
+
+   @ViewChild('map') divMap?: ElementRef;
+
   ngAfterViewInit(): void {
 
-    const map = new mapboxgl.Map({
-      container: 'map', // container ID
+   if(!this.divMap) throw 'El elemento HTML no fue encontrado'
+
+
+    const map = new Map({
+      container: this.divMap?.nativeElement, // // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
       center: [-74.5, 40], // starting position [lng, lat]
       zoom: 9, // starting zoom
--- src/app/maps/pages/markers-page/markers-page.component.ts
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -1,9 +1,77 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Map, LngLat, Marker } from 'mapbox-gl';
 
 @Component({
   templateUrl: './markers-page.component.html',
   styleUrl: './markers-page.component.css'
 })
 export class MarkersPageComponent {
+
+  @ViewChild('map') divMap?: ElementRef;
+
+
+  
+
+  public map?:Map;
+  public currentLngLat : LngLat = new LngLat(139.64420983025508, 35.85983935788241);
+
+  ngAfterViewInit(): void {
+
+   if(!this.divMap) throw 'El elemento HTML no fue encontrado'
+
+
+    this.map = new Map({
+      container: this.divMap?.nativeElement, // // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.currentLngLat, // starting position [lng, lat]
+      zoom: 13, // starting zoom
+    });
+
+
+  //   const markerHtml = document.createElement('div');
+  //   markerHtml.innerHTML= 'John '
+
+
+  //  //? Creacion de un marker
+  //   const marker = new Marker({
+  //     // color : 'red'
+  //     element : markerHtml
+  //   })
+  //   .setLngLat(this.currentLngLat)
+  //   .addTo(this.map)
+
+
+
+
+
+  }
+
+
+  //se crean en dos metodos diferentes ya que podemos usarlos en sircustancias diferentes y el create va asociado si o si al boton
+    // mientras que el add puede ir asociado a una recostruccion de localStorage
+
+
+  createMarker() {
+
+if(!this.map) return ;
+
+    const color = '#xxxxxx'.replace(/x/g, y=>(Math.random()*16|0).toString(16));
+const lngLat = this.map.getCenter();
+    this.addMarker(lngLat, color);
+  }
+
+addMarker(lngLat: LngLat, color: string){
+if (!this.map) return;
+
+const marker = new Marker({
+  color : color,
+  draggable: true
+})
+.setLngLat(lngLat)
+.addTo(this.map);
+}
+
+
+
 
 }
